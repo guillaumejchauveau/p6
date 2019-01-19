644878293b5f0af7762cef99aead3669d22ad4de
Guillaume Chauveau <guillaumejchauveau.ops.gecu@outlook.com>
2019-01-19 23:54:43 +0100

    Add Checkstyle

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
   java
   application
   jacoco
+  checkstyle
+  maven
 }
 
 application {
@@ -12,6 +14,17 @@ dependencies {
   testCompile("junit:junit:4.12")
 }
 
+checkstyle {
+  configFile = File("../checkstyle.xml")
+}
+
+tasks.withType<Checkstyle>().configureEach {
+  reports {
+    xml.isEnabled = true
+    html.isEnabled = true
+  }
+}
+
 tasks.jacocoTestReport {
   reports {
     xml.isEnabled = true
@@ -20,4 +33,11 @@ tasks.jacocoTestReport {
 
 tasks.check {
   dependsOn(tasks.jacocoTestReport)
+}
+
+task("writePom") {
+  doLast {
+    maven.pom {
+    }.writeTo("pom.xml")
+  }
 }
