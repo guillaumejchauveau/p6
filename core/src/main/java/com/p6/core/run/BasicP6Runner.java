69e55e5907886e642da63037b41d9f32fe8b6788
Guillaume Chauveau <guillaumejchauveau.ops.gecu@outlook.com>
2019-02-01 00:21:22 +0100

    Add basic P6 program execution test

--- core/src/main/java/com/p6/core/run/BasicP6Runner.java
+++ core/src/main/java/com/p6/core/run/BasicP6Runner.java
@@ -1,8 +1,39 @@
 package com.p6.core.run;
 
-class BasicP6Runner extends P6Runner {
-  @Override
-  public void run() {
+import com.p6.core.solution.Solution;
+import com.p6.core.solution.Symbol;
+import com.p6.core.solution.rule.Condition;
+import java.util.Collection;
+import java.util.Random;
 
+public class BasicP6Runner extends P6Runner {
+  private Random random;
+
+  public BasicP6Runner() {
+    this.random = new Random();
+  }
+
+  @Override
+  public void cycle(Solution solution, Integer count) {
+    for (int cycle = 0; cycle < count; cycle++) {
+      int xsymbolId = this.random.nextInt(solution.getSymbolCount());
+      int ysymbolId = this.random.nextInt(solution.getSymbolCount());
+      Symbol x = solution.getSymbol(xsymbolId);
+      Symbol y = solution.getSymbol(ysymbolId);
+      if (count - 10 < cycle) {
+        System.out.println(x);
+        System.out.println(y);
+      }
+      for (Condition condition : solution.getConditions()) {
+        if (condition.test(x, y)) {
+          Collection<Symbol> products = solution.getResult(condition).apply(x, y);
+          solution.removeSymbol(xsymbolId);
+          solution.removeSymbol(ysymbolId - ((xsymbolId < ysymbolId) ? 1 : 0));
+          for (Symbol product : products) {
+            solution.addSymbol(product);
+          }
+        }
+      }
+    }
   }
 }
--- lib/strings/src/main/java/com/p6/lib/strings/StringSolutionConfigurator.java
+++ lib/strings/src/main/java/com/p6/lib/strings/StringSolutionConfigurator.java
@@ -1,12 +1,11 @@
 package com.p6.lib.strings;
 
 import com.p6.core.solution.Solution;
-import com.p6.core.solution.SolutionConfiguration;
 import com.p6.core.solution.SolutionConfigurator;
 
 public class StringSolutionConfigurator extends SolutionConfigurator {
   @Override
-  public void configure(Solution solution, SolutionConfiguration configuration) {
+  public void configure(Solution solution) {
 
   }
 }
