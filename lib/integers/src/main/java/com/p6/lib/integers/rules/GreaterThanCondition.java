00b7d26091562460cc87f0e4fe708076ba5c9dba
Guillaume Chauveau <guillaumejchauveau.ops.gecu@outlook.com>
2019-02-01 17:17:28 +0100

    Move things around

--- lib/integers/src/main/java/com/p6/lib/integers/rules/GreaterThanCondition.java
+++ lib/integers/src/main/java/com/p6/lib/integers/rules/GreaterThanCondition.java
@@ -5,11 +5,13 @@ import com.p6.core.solution.rule.Condition;
 import com.p6.lib.integers.IntegerSymbol;
 
 public class GreaterThanCondition extends Condition {
+  @Override
   public Boolean test(Symbol x, Symbol y) {
     if (x instanceof IntegerSymbol && y instanceof IntegerSymbol) {
-      return ((IntegerSymbol) x).evaluate() > ((IntegerSymbol) y).evaluate();
+      IntegerSymbol x1 = (IntegerSymbol) x;
+      IntegerSymbol y1 = (IntegerSymbol) y;
+      return x1.evaluate() > y1.evaluate();
     }
-
     return false;
   }
 }
