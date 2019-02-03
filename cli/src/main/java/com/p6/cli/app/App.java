ca0aee922c685c23395e9935ce265ac5bb15f56e
Guillaume Chauveau <guillaumejchauveau.ops.gecu@outlook.com>
2019-02-03 17:29:11 +0100

    Add Log4j configuration

--- cli/src/main/java/com/p6/cli/app/App.java
+++ cli/src/main/java/com/p6/cli/app/App.java
@@ -3,6 +3,7 @@ package com.p6.cli.app;
 import com.p6.core.solution.SolutionConfigurator;
 import com.p6.utils.Logging;
 import java.util.ServiceLoader;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,13 +17,12 @@ public class App {
    * @param args System args
    */
   public static void main(String[] args) {
-    Logging.configureLoggingFramework();
+    Logging.configureLoggingFramework(Level.ALL);
     Logger logger = LogManager.getLogger();
-    logger.error("Test");
 
     ServiceLoader<SolutionConfigurator> loader = ServiceLoader.load(SolutionConfigurator.class);
     for (SolutionConfigurator configurator : loader) {
-      System.out.println(configurator);
+      logger.debug(configurator);
     }
   }
 }
